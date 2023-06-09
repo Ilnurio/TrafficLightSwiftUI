a6fa1892e2f77dd9a0d83663921b1ec5e0285b20
Ilnur Fattakhov <nurich1995@mail.ru>
2023-06-09 17:22:52 +0300

    add changes in logic of app

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -7,36 +7,52 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 struct ContentView: View {
+    @State private var currentLight: CurrentLight = .red
+    @State private var buttonTitle = "START"
+    
     var body: some View {
         ZStack {
             Color.black
                 .ignoresSafeArea()
-            VStack {
-                Spacer()
+            VStack(alignment: .center, spacing: 30) {
                 Text("СВЕТОФОР")
-                    .padding()
+                    .padding(5)
                     .background(.blue)
-                    .font(.title)
+                    .font(Font.title.weight(.bold))
+                    .padding()
                     .foregroundColor(Color.white)
                     .overlay(RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.blue, lineWidth: 10))
-            
-                LampSwiftUIView(color: .red)
-                LampSwiftUIView(color: .yellow)
-                LampSwiftUIView(color: .green)
-                
-                Button { } label: {
-                    Text("START")
-                        .padding()
-                        .background(.blue)
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.blue, lineWidth: 10))
+                        .stroke(Color.blue, lineWidth: 10)
+                    )
+                VStack() {
+                    LampSwiftUIView(color: .red, opacity: currentLight == .red ? 1 : 0.3)
+                    LampSwiftUIView(color: .yellow, opacity: currentLight == .yellow ? 1 : 0.3)
+                    LampSwiftUIView(color: .green, opacity: currentLight == .green ? 1 : 0.3)
                 }
-                .padding()
+                
+                Button {
+                    if buttonTitle == "START" {
+                        buttonTitle = "NEXT"
+                    }
+                    buttonTapped()
+                }
+            label: {
+                Text(buttonTitle)
+                    .padding()
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.red,Color.yellow, Color.green]), startPoint: .leading, endPoint: .trailing))
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 10)
+                    )
+            }
+            .padding()
             }
         }
     }
@@ -45,5 +61,16 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+ // MARK: - methods for buttonTapped
+extension ContentView {
+    private func buttonTapped() {
+        switch currentLight {
+        case .red: currentLight = .yellow
+        case .yellow: currentLight = .green
+        case .green: currentLight = .red
+        }
     }
 }
--- TrafficLightSwiftUI/LampSwiftUIView.swift
+++ TrafficLightSwiftUI/LampSwiftUIView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct LampSwiftUIView: View {
     let color: Color
+    let opacity: Double
     
     var body: some View {
         Circle()
             .foregroundColor(color)
             .frame(width: 150, height: 150)
+            .opacity(0.3)
             .overlay(Circle()
                 .stroke(Color.white, lineWidth: 4))
     }
@@ -21,6 +23,6 @@ struct LampSwiftUIView: View {
 
 struct LampSwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        LampSwiftUIView(color: .red)
+        LampSwiftUIView(color: .red, opacity: 1)
     }
 }
